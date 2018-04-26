2b095d5106766c62dd658ae01d0eb11aa9d09ade
mrghost <ali.dragon.swr@gmail.com>
2018-04-26 19:30:28 +0430

    testing out reducers

---
+++ src/reducer/someRed.js
@@ -0,0 +1,24 @@
+var def = {
+  users: [{
+    name: 'ali',
+    age: '123',
+    createdAt: 45459822,
+    id: 1
+  }]
+}
+
+export default function someRed(state = def, action){
+  switch(action.type){
+    case 'ADD':
+    return {
+      ...state,
+      users: [
+        ...state.users,
+        {
+          ...action.user,
+          id: state.users.length + 1
+        }
+      ]
+    }
+  }
+}
