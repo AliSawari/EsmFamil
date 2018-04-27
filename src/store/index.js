113d426d5b883eb9cb15d7a35516dc719562cd64
mrghost <ali.dragon.swr@gmail.com>
2018-04-27 20:52:38 +0430

    testing out

--- src/store/index.js
+++ src/store/index.js
@@ -3,4 +3,9 @@ import reducer from './../reducer';
 
 const store = createStore(reducer);
 
+var updates = store.subscibe(() => {
+  let state = store.getState();
+  console.log('State :: ', state);
+});
+
 export default store;
