f3618b0bf7d06426716cf8b2d4ecc6ce4e4eb0c4
mrghost <ali.dragon.swr@gmail.com>
2018-05-09 18:59:01 +0430

    error handling done

--- src/components/Origin.js
+++ src/components/Origin.js
@@ -7,7 +7,9 @@ class Origin extends Component {
 
     this.state = {
       userList: [],
-      myName: undefined
+      myName: undefined,
+      alerted: false,
+      alertMsg: undefined
     }
 
     socket.on('update', (users) => {
@@ -17,13 +19,43 @@ class Origin extends Component {
     });
 
     socket.on('join_success', (name) => {
-      console.log("Success");
       this.setState({myName: name});
+      this.setState({alerted: 'success', alertMsg: "Successfuly Joined!"});
     });
 
 
     // binders :
     this.mapUsers = this.mapUsers.bind(this);
+    this.alert = this.alert.bind(this);
+  }
+
+
+  alert(){
+    let {alerted, alertMsg} = this.state;
+
+
+    const clearAlert = () => {
+      setTimeout(() => {
+        this.setState({
+          alerted: false, alertMsg: undefined
+        });
+      }, 5000);
+    }
+
+
+    if(alerted){
+      if(alerted === 'success'){
+        clearAlert();
+        return <div className="alert alert-success" role="alert">
+          <b>{alertMsg}</b>
+        </div>
+      } else if(alerted === 'fail'){
+        clearAlert();
+        return <div className="alert alert-danger" role="alert">
+          <b>{alertMsg}</b>
+        </div>
+      }
+    }
   }
 
   mapUsers(){
@@ -38,12 +70,14 @@ class Origin extends Component {
 
   render(){
     let {myName, userList} = this.state;
+
     return <div className="jumbotron">
       <h1>EsmFamil</h1>
       {myName ? <h3>Welcome to the game <b>{myName}</b></h3> : <h3>Please choose a name</h3>}
+      {this.alert()}
       <hr />
         <button type="button" className="btn btn-primary">
-          Online <span className="badge badge-light">{userList ? userList.length : 0}</span>
+          Online <span className="badge badge-light">{userList.length}</span>
         </button>
         <h3>Players list : </h3>
         <ul className="list-group">
